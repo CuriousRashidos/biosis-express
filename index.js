7fe81711ce5fd2841fdc1377f265280f7edf47a4
unknown <vergil1998@hotmail.com>
2020-11-08 14:52:47 +0400

    added products and services heading

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false,
+});
+
+//airtable config
+var Airtable = require("airtable");
+var base = new Airtable({
+  //TODO: put key in environment variable
+  apiKey: "keytNjOg371DOg3Mm",
+}).base("app2lFfv9F779uihP");
 
 //static files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname+'/public/css'))
-app.use('/img', express.static(__dirname+'/public/img'))
-app.use('/js', express.static(__dirname+'/public/js'))
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/src/public/css"));
+//not using this since all images are src-ed from imgur 
+app.use("/img", express.static(__dirname + "/src/public/img"));
+app.use("/js", express.static(__dirname + "/src/public/js"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
-})
+//root
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/views/index.html");
+});
 
-app.listen(process.env.PORT, () => {
-    console.log('server running on ', process.env.PORT)
-})
+// /submit
+app.post("/submit", urlencodedParser, async (req, res) => {
+  //form data
+  const { name, email, phone, message } = req.body;
+  //create table
+  base("contactDetails").create(
+    [
+      {
+        fields: {
+          name,
+          email,
+          phone,
+          message,
+        },
+      },
+    ],
+
+    //debug
+    function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record) {
+        console.log(record.getId());
+      });
+    }
+  );
+  res.redirect("/");
+});
+
+app.listen(9000);
